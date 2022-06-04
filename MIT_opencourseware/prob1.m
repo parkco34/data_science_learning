4c3c7ea6b909efda37564639a5e57e9f2ddd805d
parkco34 <parkercorya@gmail.com>
2022-06-04 15:15:20 -0400

    Write a MATLAB script that calculates the mean and median of a sample of 100 uniform randonumbers between 0 and 2 and the percentage of points in the sample that are greater than 1m

---
+++ MIT_opencourseware/prob1.m
@@ -0,0 +1,17 @@
+%% Write a MATLAB script that calculates the mean and median of a sample of 100 uniform random
+% numbers between 0 and 2 and the percentage of points in the sample that are greater than 1
+
+function [avg, mid] = prob1(N, a, b)
+% ==================================================================
+% INPUTS:
+%   N = (1xN double) Uniform Random numbers
+%   a, b = (1x1 double) Range of values between a and b, where a < b
+%
+% OUTPUTS:
+%   avg = (1x1 double) Mean
+%   mid = (1x1 double) Median, where if the number of values is EVEN, the
+%   middle two will be AVERAGED
+% ==================================================================
+
+    
+end
